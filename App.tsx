b17470f0dabfff0708ea960d89340177321ed443
Uways <uwaysz@gmail.com>
2024-03-22 11:36:49 +0800

    Example button input

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+} from "react-native";
 
 export default function App() {
+  const { height } = useWindowDimensions();
+  const [number, setNumber] = useState(0);
+
+  const handlePress = () =>
+    setNumber(parseInt((Math.random() * 10000).toString(), 10) % 100);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { height }, StyleSheet.absoluteFill]}>
+      <Text>Random number: {number}</Text>
+      <View style={styles.br} />
+      <Pressable
+        style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1 }, styles.btn]}
+        onPress={handlePress}
+      >
+        <Text style={styles.btnText}>Generate a number</Text>
+      </Pressable>
     </View>
   );
 }
@@ -13,8 +31,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  br: {
+    height: 12,
+  },
+  btn: {
+    backgroundColor: "#222",
+    padding: 10,
+  },
+  btnText: {
+    color: "#fff",
   },
 });
